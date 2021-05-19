fe024048bfe4fe67abb778b6327bef6da97d7010
manbalboy <manbalboy@hanmail.net>
2021-05-19 14:03:49 +0900

    [frontend][manbalboy] frontend eslint 설정

--- frontend/invitation/.eslintrc.js
+++ frontend/invitation/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    jest: true
   },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
@@ -10,6 +11,22 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": ['error', {
+      singleQuote: true,
+      semi: true,
+      useTabs: false,
+      tabWidth: 4,
+      trailingComma: 'all',
+      printWidth: 120,
+      bracketSpacing: true,
+      arrowParens: 'avoid', //가능하면 생략 , always 항상 써야한다.
+      proseWrap : 'preserve', 
+      jsxBracketSameLine : false ,
+      htmlWhitespaceSensitivity: "strict",
+      vueIndentScriptAndStyle :true, // script 영역의 들여쓰기  true false
+      endOfLine: 'auto'
+      // insertPragma: true
+    }]
   },
   overrides: [
     {
